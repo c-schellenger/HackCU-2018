0f609ffce2a1a2e3704da4b228256adad642f73b
Benjamin Lin <dekourier@gmail.com>
2018-02-24 14:43:23 -0700

    added json query parsing

--- AlteryxChallenge/main.py
+++ AlteryxChallenge/main.py
@@ -1,10 +1,5 @@
 import pandas as pd
-
-
-
-
-
-
+import urllib.request, json
 
 dataFile = open("rawdata.txt", 'r')
 
@@ -32,6 +27,42 @@ str1 = ''.join(str(e) for e in chars)
 cleaned_data = str1.split(',')
 print(cleaned_data)
 
+d = {'url' : cleaned_data[0::3], 'query' : cleaned_data[1::3], 'value' : cleaned_data[2::3]}
 
-"""df1 = pd.read_json("https://data.cityofnewyork.us/api/views/kku6-nxdu")
-print(df1)"""
+df = pd.DataFrame(d)
+print(df)
+
+decrypted = []
+
+#"""
+for index, row in df.iterrows():
+    url = row['url']
+    query = row['query'].split(".")
+    print(url)
+    print(query)
+    with urllib.request.urlopen(url) as url:
+        jsondata = json.loads(url.read().decode())
+        """query_result = jsondata[query[0]]
+        query.pop(0)
+        #print(query_result)
+        print(query)"""
+        while query:
+            #try:
+            print(query)
+            #query_result = jsondata[int(query[0])]
+            #except ValueError:
+            query_result = jsondata[query[0]]
+            query.pop(0)
+            print(query_result)
+        decrypted.append(query_result)
+"""
+with urllib.request.urlopen("https://data.cityofnewyork.us/api/views/kku6-nxdu") as url:
+        jsondata = json.loads(url.read().decode())
+        print(jsondata)
+        print(jsondata["columns"])
+        print(jsondata["columns"]["0"])
+        print(jsondata["columns"][0]["cachedContents"])
+        print(jsondata["columns"][0]["cachedContents"]["top"])
+        print(jsondata["columns"][0]["cachedContents"]["top"][16])
+        print(jsondata["columns"][0]["cachedContents"]["top"][16]["count"])
+"""
